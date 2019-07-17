9af2180848359faa4e41b0619c1d6d90742b5b36
Vituriano <viturianooliveira@gmail.com>
2019-07-17 04:42:56 -0400

    git ignore

--- app.js
+++ app.js
@@ -2,27 +2,29 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 
-const data = `  <html>
-                    <head>
-                        <title>Html2PDF Learn<title>
-                    </head>
+const data = `  
+<html>
+    <head>
+        <title>Html2PDF Learn<title>
+    </head>
 
-                    <body>
-                        <h1>Esse é o título</h1>
-                        <p>
-                            Esse documento em PDF foi gerado a partir de um código HTML usando NodeJS;
-                        </p>
-                    </body>
-                </html> `;
+    <body>
+        <h1>Esse é o título</h1>
+        <p>
+            Esse documento em PDF foi gerado a partir de um código HTML usando NodeJS;
+        </p>
+    </body>
+</html> `;
 
 //gera html
 fs.writeFile(`./HTML/newFile.html`, data , function (erro) {
     if (erro) throw erro;
 });
 //ler html
-//var html = fs.readFileSync(`./HTML/newFile.html`, 'utf8');
-//var options = { format: 'A4', orientation: 'portrait' };
- //gera PDF a partir do html
+var html = fs.readFileSync(`./HTML/newFile.html`, 'utf8');
+var options = { format: 'A4', orientation: 'portrait' };
+console.log(html);
+//gera PDF a partir do html
 // pdf.create(html, options).toFile(`./Docs/newFile.pdf`, function (err, res) {
 //     if (err) throw erro;
 // });
