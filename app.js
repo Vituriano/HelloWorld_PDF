96e957c9e6e6aede1e961f2551a47957353cb20b
Vituriano <viturianooliveira@gmail.com>
2019-07-17 04:39:07 -0400

    l

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-// Servidor
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-//setar html
-const path = require("path");
-//body parser
-const bodyParser = require('body-parser');
 //Gerador de PDF
 var fs = require('fs');
 var pdf = require('html-pdf');
-
-//seta html
-app.use(express.static(path.join(__dirname, 'HTML')));
-app.set("views", path.join(__dirname, 'HTML'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-//Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 
 const data = `  <html>
                     <head>
@@ -39,11 +20,9 @@ fs.writeFile(`./HTML/newFile.html`, data , function (erro) {
     if (erro) throw erro;
 });
 //ler html
-var html = fs.readFileSync(`./HTML/newFile.html`, 'utf8');
+//var html = fs.readFileSync(`./HTML/newFile.html`, 'utf8');
 //var options = { format: 'A4', orientation: 'portrait' };
  //gera PDF a partir do html
-pdf.create(html, options).toFile(`./Docs/newFile.pdf`, function (err, res) {
-    if (err) throw erro;
-});
-
-server.listen(3000);
+// pdf.create(html, options).toFile(`./Docs/newFile.pdf`, function (err, res) {
+//     if (err) throw erro;
+// });
